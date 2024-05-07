b0eeb07361f064cd028939e103e7cd6cbca1793d
FrankIzu <francis.onodueze@gmail.com>
2024-05-07 15:41:09 -0400

    logs changes

--- testDI.py
+++ testDI.py
@@ -3,4 +3,4 @@ from data_implantation_frontier.DI_Frontier_TSR_multiclass import DataImplantati
 
 dataset = pd.read_csv("datasets/cleaned.csv")
 process = DataImplantation_Frontier() # provide name of your label column; Do you want to separate your testset?
-df, _, _ =  process.runDIprocess(dataset, 'FLAG', di_ratio=20) # _ are for none needed return values
+df, _, _ =  process.runDIprocess(dataset, 'FLAG', di_ratio=100) # _ are for none needed return values
